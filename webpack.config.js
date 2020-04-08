842dcc9f9fcaa1ce9a50ae3a0266ec1f0f80f75b
Sydney Barovsky <sbarovsky@gmail.com>
2020-04-08 14:05:07 +0800

    added eslint and started webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: {
+    main: './assets/js/index.js'
+  },
+  module: {
+    rules: [
+    { test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'] } } },
+    { test: /\.(sa|sc|c)ss$/,
+      use: [
+        { loader: MiniCssExtractPlugin.loader },
+        { loader: 'css-loader', },
+        { loader: 'postcss-loader' },
+        {
+          loader: 'sass-loader',
+          options: { implementation: require('sass') } } ] } ] },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'bundle.css' }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ title: 'Output Management', }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+};
