8519bc8c0ad046efff2b5793a3f10ae85a9a245c
Sydney Barovsky <sbarovsky@gmail.com>
2020-04-09 04:25:20 +0800

    split webpack dev and production configurations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.scss';
+import './style.scss';  // import main SASS FILE
 import _ from 'lodash';
 
 // IMPORT LIBRARIES
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,7 @@
-const path = require('path');
-const extract = require("mini-css-extract-plugin");
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack_config/webpack.common.js')
 
-module.exports = {
-  entry: {
-    main: './src/index.js'
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-    { test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'] } } },
-    { test:/\.(sa|sc|c)ss$/,
-      use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader',
-            options: { implementation: require('sass') }
-          } ]
-    },
-    { test: /\.(png|jpe?g|gif|svg)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            outputPath: "images"
-    } } ] } ] },
-  plugins: [
-    new ErrorOverlayPlugin(),
-    new CleanWebpackPlugin(),
-    new HTMLWebpackPlugin({
-      title: 'Sydney Barovsky Portfolio'
-    }),
-    new extract({
-      filename: 'bundle.css',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/'
-    })
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
+module.exports = ({ env }) => {
+  const enviro = require(`./webpack_config/webpack.${env}.js`);
+  return merge(common, enviro);
 };
