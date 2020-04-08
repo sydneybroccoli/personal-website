7c228a7c54ddfd60d8cce39063b99b5fc61474f4
Sydney Barovsky <sbarovsky@gmail.com>
2020-04-09 05:45:15 +0800

    adding favicon resource

--- webpack_config/webpack.prod.js
+++ webpack_config/webpack.prod.js
@@ -3,9 +3,6 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  devServer: {
-    contentBase: './dist',
-  },
   plugins: [
     require('autoprefixer'),
     require('cssnano')
