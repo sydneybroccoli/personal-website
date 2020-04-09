b275dc1022b81d765f745c210ad4564ba5d3fee5
Sydney Barovsky <sbarovsky@gmail.com>
2020-04-09 15:19:08 +0800

    getting all my files back!

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// IMPORT FILES
+
+// IMPORT STYLE
+import './style.scss';  // import main SASS FILE
+
+// IMPORT JS LIBRARIES
+import _ from 'lodash';
+import { loadParticles } from '../assets/javascript/scripts/init_particles.js'
+
+// IMPORT JS PLUGINS
+// IMPORT OTHER JS SCRIPTS
+
+// DEVELOPMENT MODE?
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
+// RUN FUNCTIONS
+document.addEventListener("DOMContentLoaded", function () {
+  loadParticles();
+});
+
+console.log('hello from webpacker!');
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const merge = require('webpack-merge');
+const common = require('./webpack_config/webpack.common.js')
+
+module.exports = ({ env }) => {
+  const enviro = require(`./webpack_config/webpack.${env}.js`);
+  return merge(common, enviro);
+};
