73f70547a86e217acf5a6e7b036821993e38940a
Sydney Barovsky <sbarovsky@gmail.com>
2020-04-11 04:25:46 +0800

    setup nav bar w/ js to switch between "pages"

--- src/index.js
+++ src/index.js
@@ -5,19 +5,21 @@ import './style.scss';  // import main SASS FILE
 
 // IMPORT JS LIBRARIES
 import _ from 'lodash';
-import { loadParticles } from '../assets/javascript/scripts/init_particles.js'
 
 // IMPORT JS PLUGINS
 // IMPORT OTHER JS SCRIPTS
+import { loadParticles } from '../assets/javascript/scripts/init_particles.js';
+import { navigationSelector } from '../assets/javascript/scripts/navigator.js';
 
 // DEVELOPMENT MODE?
 if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
+  console.log('looks like we are in development mode!');
 }
 
 // RUN FUNCTIONS
 document.addEventListener("DOMContentLoaded", function () {
   loadParticles();
+  navigationSelector();
 });
 
 console.log('hello from webpacker!');
