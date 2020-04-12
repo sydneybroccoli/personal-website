2704600945f064f2eb97129b48ab05714134a2ed
Sydney Barovsky <sbarovsky@gmail.com>
2020-04-12 12:07:06 +0800

    updated favicon - does not appear to have changed in development? jk it has changed, the browser just needs to be restarted. also renamed all partials to include underscore and changed the webpack config

--- webpack_config/webpack.common.js
+++ webpack_config/webpack.common.js
@@ -18,7 +18,7 @@ module.exports = {
   module: {
     rules: [
     // HTML PARTIALS RULES
-    { test: /\.(html)$/,
+    { test: /_.*\.(html)$/,
       include: path.resolve(__dirname, './assets/pages'),
       use: {
         loader: 'html-loader',
