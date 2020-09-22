37889a037190f327e53915e27ae4aae06830ef92
Sydney Barovsky <sbarovsky@gmail.com>
2020-09-21 20:30:17 -0700

    project modals working but need some fixing

---
+++ assets/javascript/scripts/portfolio/project_modal.js
@@ -0,0 +1,13 @@
+import { toggleModal } from '../modalActions.js';
+
+export const projects_toggleModal = () => {
+  const modals = document.querySelectorAll('.projModal');
+
+  modals.forEach( (modal) => {
+    const proj_id = modal.id;
+    const openBtn = document.querySelector(`#${proj_id}_openBtn`);
+    const closeBtn = document.querySelector(`#${proj_id}_closeBtn`);
+
+    toggleModal( modal, openBtn, closeBtn );
+  });
+}
